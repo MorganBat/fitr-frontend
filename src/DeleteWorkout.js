4f5a79c9290d82e6a6f5bc02e0e64953587f54aa
GARY <gary.v.ho@gmail.com>
2020-07-26 13:11:14 +1000

    edit and delete changes

---
+++ src/DeleteWorkout.js
@@ -0,0 +1,25 @@
+import react from 'react'
+import axios from 'axios'
+
+
+const DeleteWorkout = ({workoutId, onDelete}) => {
+
+    const token = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJleHAiOjE1OTU3ODYzNzksInN1YiI6MSwiZW1haWwiOiJ0ZXN0MUBnbWFpbC5jb20ifQ.MXeqITDxtZBZPkhVF0vhvNRZuVDzZnCRNs_VO3QK4lk'
+
+    function DeleteWorkout() {
+        axios.delete(`https://fitr-backend.herokuapp.com/workouts/${workoutId}`, {
+            headers: {
+                'Authorization': `Bearer ${token}`
+            }
+        })
+        .then(onDelete())
+    }
+
+
+    return (
+        <button onClick={DeleteWorkout}> Delete </button>
+    )
+}
+
+export default DeleteWorkout
+
--- src/editWorkout.js
+++ src/editWorkout.js
@@ -2,28 +2,52 @@ import React, {useState, useEffect} from 'react'
 import axios from 'axios'
 import {Redirect} from 'react-router-dom'
 
-const EditWorkout = (props) => {
+const EditWorkout = ({workoutId}) => {
 
     const [workout, setWorkout] = useState("")
+    const [workoutDate, setWorkoutDate] = useState("")
+    const [workoutUser, setWorkoutUser] = useState("")
     const [isEdited, setIsEdited] = useState(false)
     const [errorMessage, setErrorMessage] = useState("")
 
     const token = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJleHAiOjE1OTU3ODYzNzksInN1YiI6MSwiZW1haWwiOiJ0ZXN0MUBnbWFpbC5jb20ifQ.MXeqITDxtZBZPkhVF0vhvNRZuVDzZnCRNs_VO3QK4lk'
 
     useEffect(() => {
-        axios.get(`https://fitr-backend.herokuapp.com/workouts/${props.workoutId}`, {
+        axios.get(`https://fitr-backend.herokuapp.com/workouts/${workoutId}`, {
             headers: {
                 'Authorization': `Bearer ${token}`
             }    
         })
         .then(res => {
+            setWorkoutDate(res.data.date)
             setWorkout(res.data.exercises)
+            setWorkoutUser(res.data.user)
         })
-    })
+        .catch(e => {
+            setErrorMessage("el problemo")
+        })
+    }, [])
+
+    function editWorkout() {
+        axios.put(`https://fitr-backend.herokuapp.com/workouts/${workoutId}`,{
+            /*workout format*/},{
+                headers: {
+                    'Authorization': `Bearer ${token}`
+                }   
+            }
+        })
+    }
 
 return (
     <div>
-
+        {errorMessage && <h3>{errorMessage}</h3>}
+        {
+            workout: {
+                exercises: "earaer",
+                date: "year-month-day",
+                user:
+            }
+        }
     </div>
 )
 
