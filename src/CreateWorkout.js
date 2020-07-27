124e0b1ccfb3a300bc48a0bf81f0833c2cb64254
GARY <gary.v.ho@gmail.com>
2020-07-27 15:05:09 +1000

    more state fixes

--- src/CreateWorkout.js
+++ src/CreateWorkout.js
@@ -23,27 +23,39 @@ const CreateWorkout = ({jwt}) => {
 
     function addExercise() {
         setExercises([...exercise, ""])
-        console.log(exercise)
       }
     
       function handleChange(e, index) {
         exercise[index] = e.target.value
+        setExercises([...exercise])
+      }
     
+      function handleRemove(index){
+        exercise.splice(index, 1)
+        setExercises([...exercise])
+      }
+    
+      function consoleLog(){
+        console.log(exercise)
       }
     
 
-    return (
+      return (
         <div>
           <h1>Record a workout</h1>
-            {exercise.map((exercise,index) =>(
+            {exercise.map((ex,index) =>(
                 <>
                 <div key={index}>
-                  <input type="text" onChange={(e) => handleChange(e, index) } defaultValue={exercise}/>
+                  <input type="text" onChange={(e) => handleChange(e, index) } value={ex}/>
+                  <button onClick={() => handleRemove(index)}>Remove</button>
                 </div>
                 </>
              ))}
            
           <button onClick={addExercise}>Add Exercise</button>
+    
+          <hr/>
+          <button onClick={consoleLog}>LOG</button>
     
           <button>Submit</button>
         </div>
