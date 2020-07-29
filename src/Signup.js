ded385f61fa43c103be136ff133c555e6b341950
Morgan Batterham <morgan@morganbatterham.me>
2020-07-29 18:19:56 +1000

    changes to signup form

--- src/Signup.js
+++ src/Signup.js
@@ -8,18 +8,20 @@ const [userEmail, setEmail] = useState("")
 const [userPassword, setPassword] = useState("")
 const [isCreated, setIsCreated] = useState(false)
 const [errorMessage, setErrorMessage] = useState("")
+const [userName, setUserName] = useState("")
 
     function createUser() {
-        if(userEmail && userPassword) {
+        if(userName && userEmail && userPassword) {
             axios.post('https://fitr-backend.herokuapp.com/users', {
                 user: {
+                    name: userName,
                     email: userEmail,
                     password: userPassword
                 }
             })
             .then(setIsCreated(true))
         } else {
-            setErrorMessage("Please enter a username and password")
+            setErrorMessage("Please fill out the entire form")
         }
     }
 
@@ -27,18 +29,30 @@ const [errorMessage, setErrorMessage] = useState("")
         <div className="grid-container">
             <h1>SIGNUP</h1>
             <div>{errorMessage}</div>
+            <div>
+            <input
+                placeholder="Name"
+                type="text"
+                value={userName}
+                onChange={e => setUserName(e.target.value)}
+            />
+            </div>
+            <div>
             <input
                 placeholder="Email"
-                type="text"
+                type="email"
                 value={userEmail}
                 onChange={e => setEmail(e.target.value)}
             />
+            </div>
+            <div>
             <input
                 placeholder="Password"
                 type="password"
                 value={userPassword}
                 onChange={e => setPassword(e.target.value)}
             />
+            </div>
             <button onClick={createUser} class="btn btn-success"> Signup </button>
             <br />
             <Link to="/login">Already have an account? Login</Link>
