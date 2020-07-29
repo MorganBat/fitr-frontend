957b4232618ed519ea6b64dfbdae304dc2035da2
Morgan Batterham <morgan@morganbatterham.me>
2020-07-29 18:21:07 +1000

    changes to login form styling

--- src/Login.js
+++ src/Login.js
@@ -24,17 +24,21 @@ async function getToken() {
 return (
     <div>
         <h1>LOG IN</h1>
+    <div>
     <input
         placeholder="Email"
         value={userEmail}
         onChange={e => setUserEmail(e.target.value)}
     />
+    </div>
+    <div>
     <input
         type="password"
         placeholder="Password"
         value={userPassword}
         onChange={e => setUserPassword(e.target.value)}
     />
+    </div>
     <button onClick={getToken} class="btn btn-success"> Login </button>
     <br />
     <Link to="/signup">Need an Account? Register Now</Link>
