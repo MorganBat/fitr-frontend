971d0ff0100fc3cf4747b207999a97c9c7d34504
Gary <gary.v.ho@gmail.com>
2020-07-30 15:55:29 +1000

    init commit

---
+++ src/Splash.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import {Link} from 'react-router-dom'
+
+const Splash = () => {
+
+
+
+
+    return (
+        <div className>
+
+        </div>
+    )
+}
+
+export default Splash
