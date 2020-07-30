199f56ebb17995918d0496248e67c1179e0a8b16
Morgan Batterham <morgan@morganbatterham.me>
2020-07-30 13:10:45 +1000

    testing work

--- cypress/integration/add_workout_spec.js
+++ cypress/integration/add_workout_spec.js
@@ -4,5 +4,7 @@ describe('The Add Workout test', () => {
       cy.get('[placeholder="Email"]').type("test4@gmail.com") // Insert email
       cy.get('[placeholder="Password').type("test4") // Insert password
       cy.contains("Login").click() // Click login button
+      cy.contains("Add a new Workout").click() // Clicks the create workout link
+      cy.contains("Add Exercise").click() // Clicks to add a new exercises
     })
   })
